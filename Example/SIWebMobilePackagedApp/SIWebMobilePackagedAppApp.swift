941a5842f5387b5ca347951a37a42ace03107b74
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-07-04 22:29:25 +0530

    Added Example Project for Package.

---
+++ Example/SIWebMobilePackagedApp/SIWebMobilePackagedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIWebMobilePackagedAppApp.swift
+//  SIWebMobilePackagedApp
+//
+//  Created by Nausheen Khan on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SIWebMobilePackagedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
