d5b3700834afba35ab92d4f78904d3820ffc9864
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-07-06 13:11:47 +0530

    Unable to initialise the model.

--- Example/SIWebMobilePackagedApp/ContentView.swift
+++ Example/SIWebMobilePackagedApp/ContentView.swift
@@ -11,7 +11,6 @@ import SIScoresStrip
 struct ContentView: View {
     var body: some View {
         VStack {
-            
         }
     }
 }
--- Sources/SIScoresStrip/Views/ScoreStripBasic.swift
+++ Sources/SIScoresStrip/Views/ScoreStripBasic.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct ScoreStripBasic: View {
-    private var scoresStripBasicModel: ScoresStripBasicModel
+    public var scoresStripBasicModel: ScoresStripBasicModel
     
     public init(scoresStripBasicModel: ScoresStripBasicModel) {
         self.scoresStripBasicModel = scoresStripBasicModel
