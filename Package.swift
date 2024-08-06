802a03aaf0520c221cec5e479e14ed62a377fe69
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-08-07 02:03:22 +0530

    added back si networking dependency.

--- Package.swift
+++ Package.swift
@@ -15,13 +15,14 @@ let package = Package(
             targets: ["SIScoresStrip"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/si-ajinkya-sonar/SINetworking", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SIScoresStrip",
-            dependencies: [],
+            dependencies: ["SINetworking"],
             path: "Sources",
             resources: [
                 .process("Resources")
